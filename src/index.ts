ed8424b7a4dae2a7a4d131bf47b451c0f3ae7065
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-22 16:03:52 -0300

    chore: insere setTimeOut para exemplo assíncrono

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,26 @@
 import { from, Observable, Subscriber } from "rxjs";
 
-/* Teste com RXJS */
-let numbers = [1, 5, 10];
-/* From: função que retorna um observable (recebe atualizações, inscrição)*/
-let source = from(numbers);
+let numbers = [1, 5, 10, 15, 20, 25, 30];
 
-/* A função será chamada quando o observable quando alguém realizar um subscribe e retornará um array*/
-let source1 = new Observable(Subscriber => {
-    Subscriber.next(numbers);
-})
+let source = new Observable(Subscriber => {
 
-/* A função será chamada quando o observable quando alguém realizar um subscribe e retornará números*/
-let sourceInstance = new Observable(Subscriber => {
-    for (let n of numbers) {
-        Subscriber.next(n);
-    }
-    Subscriber.complete();
-})
-
-/* Teste com erro*/
-let sourceInstanceError = new Observable(Subscriber => {
-    for (let n of numbers) {
-        if (n > 5) {
-            Subscriber.error('Aconteceu um erro esperado!')
+    let index = 0;
+    let produceValue = () => {
+        /* Exemplo de chamada recursiva assíncrona*/
+        Subscriber.next(numbers[index++])
+        if (index < numbers.length) {
+            setTimeout(produceValue, 1000)
+        } else {
+            // Subscriber.error('Aconteceu um erro esperado');
+            Subscriber.complete();
         }
-        Subscriber.next(n);
+
     }
-    Subscriber.complete();
+    produceValue();
 })
 
-/* Exemplo da implementação de um Observer JSON*/
-let myObserver = {
+source.subscribe({
     next: (x: number) => console.log(x),
     error: (e: Error) => console.log(e),
     complete: () => console.log('Completed'),
-}
-
-function component() {
-    /* Só é possível trazer resultados com subscribe, se não houver nenhum erro */
-    source.subscribe({
-        /* Função "next" responsável por executar */
-        next: (x) => console.log(myObserver)
-    });
-
-    /* Teste com subscriber, não passa em item por item, retorna um array*/
-    source1.subscribe(myObserver)
-
-    /* Teste com subscriber passando item por item*/
-    sourceInstance.subscribe(myObserver)
-
-    /* Teste com erro - Erro sempre para um subscribe*/
-    sourceInstanceError.subscribe(myObserver)
-}
-
-component();
+})
