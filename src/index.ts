b6d3c6f64b61b57fd4eabce296f4c7f051b95e2b
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-22 17:01:07 -0300

    feat: insere XMLHttp request para exemplos

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
 import { fromEvent } from "rxjs";
 import { map, filter, delay } from "rxjs/operators";
 
-interface mouseTrack {
-    x: number;
-    y: number;
+interface IMovie {
+    title: string;
 }
 
-let circle = document.getElementById('circle');
-/* Exemplo que trabalha o retorno dos observables */
-let source = fromEvent(document, 'mousemove').pipe(
-    map((e: MouseEvent) => {
-        return { x: e.clientX, y: e.clientY }
-    }),
-    filter((value: mouseTrack) => value.x < 500),
-    delay(500)
-)
+let button = document.getElementById('button');
+let output = document.getElementById('output');
+
+let click = fromEvent(button, 'click');
 
 
-function onNext(value: mouseTrack) {
-    console.log(value);
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+        let movies = JSON.parse(xhr.responseText);
+
+        movies.forEach((movie: IMovie) => {
+            let div = document.createElement('div');
+            div.innerText = movie.title;
+            output.appendChild(div);
+        });
+    })
+    xhr.open('GET', url)
+    xhr.send()
 }
 
-source.subscribe({
-    next: (value: mouseTrack) => onNext(value),
+click.subscribe({
+    next: () => load('../movies.json'),
     error: (e: Error) => console.log(e),
     complete: () => console.log()
 })
