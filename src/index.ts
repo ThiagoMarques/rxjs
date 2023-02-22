a3b939b356c38c3b5ed00ec204afda4dab43e89e
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-22 15:52:02 -0300

    chore: exemplo de um subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import { from } from "rxjs";
+import { from, Observable, Subscriber } from "rxjs";
 
 /* Teste com RXJS */
 let numbers = [1, 5, 10];
 /* From: função que retorna um observable (recebe atualizações, inscrição)*/
 let source = from(numbers);
 
-/* Exemplo de um Observer Classe*/
-class myObserver {
-    next(x: number) {
-        console.log(x)
-    }
-    error(e: Error) {
-        console.log(e)
-    }
-    complete() {
-        console.log('Completed')
-    }
-}
+/* A função será chamada quando o observable quando alguém realizar um subscribe e retornará um array*/
+let source1 = new Observable(Subscriber => {
+    Subscriber.next(numbers);
+})
 
-/* Exemplo de um Observer JSON*/
-let myObserverJSON = {
+/* A função será chamada quando o observable quando alguém realizar um subscribe e retornará números*/
+let sourceInstance = new Observable(Subscriber => {
+    for (let n of numbers) {
+        Subscriber.next(n);
+    }
+    Subscriber.complete();
+})
+
+/* Teste com erro*/
+let sourceInstanceError = new Observable(Subscriber => {
+    for (let n of numbers) {
+        if (n > 5) {
+            Subscriber.error('Aconteceu um erro esperado!')
+        }
+        Subscriber.next(n);
+    }
+    Subscriber.complete();
+})
+
+/* Exemplo da implementação de um Observer JSON*/
+let myObserver = {
     next: (x: number) => console.log(x),
     error: (e: Error) => console.log(e),
     complete: () => console.log('Completed'),
@@ -29,8 +40,17 @@ function component() {
     /* Só é possível trazer resultados com subscribe, se não houver nenhum erro */
     source.subscribe({
         /* Função "next" responsável por executar */
-        next: (x) => console.log(new myObserver)
-    })
+        next: (x) => console.log(myObserver)
+    });
+
+    /* Teste com subscriber, não passa em item por item, retorna um array*/
+    source1.subscribe(myObserver)
+
+    /* Teste com subscriber passando item por item*/
+    sourceInstance.subscribe(myObserver)
+
+    /* Teste com erro - Erro sempre para um subscribe*/
+    sourceInstanceError.subscribe(myObserver)
 }
 
 component();
