fd4673191f1d24927eb6916996d3692529ac6011
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-23 09:47:29 -0300

    feat: insere sidenav dinâmica com RxJS para controle de tamanho de tela

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Material';
+  /* !: pode ser nulo ou não */
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  showFiller = false;
+  constructor(private breakpoint: BreakpointObserver) { }
+
+  ngAfterContentInit(): void {
+    this.breakpoint.observe(['(max-width: 800px)']).subscribe({
+      next: (res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      }
+    })
+
+  }
 }
