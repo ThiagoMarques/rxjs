1c19fcb135fabd01d0ce3033dfab9271f1e8e5c7
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-24 11:48:09 -0300

    feat: inclui scroll sombreado no header com RxJS

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isSmallScreen = false;
+export class AppComponent implements OnInit {
+  public isSmallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+
   constructor(private breakpoint: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER);
+    console.log(content[0])
+    fromEvent(content[0], 'scroll')
+      /* Uso do pipe para evitar pegar o retorno do subscribe */
+      .pipe(
+        map(() => content[0].scrollTop)
+      )
+      .subscribe({
+        /* Resultado do map é passado para next */
+        next: (value) => this.determineHeader(value)
+      })
+  }
+  /* top: distância do topo */
+  determineHeader(top: number) {
+    this.popText = top >= TEXT_LIMIT;
+    this.applyShadow = top >= SHADOW_LIMIT
+  }
 
   ngAfterContentInit(): void {
     /* Trabalhando com operador do RxJS para eliminar if - subscribe direto*/
