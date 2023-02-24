55ac4563525c86ef4a37b5503580cbe785c539fc
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-24 12:43:01 -0300

    feat: insere progressbar and spinner

--- Angular-Material/src/app/Material/Material.module.ts
+++ Angular-Material/src/app/Material/Material.module.ts
@@ -6,11 +6,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
-  exports: [MatSlideToggleModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatInputModule, MatListModule],
+  exports: [MatSlideToggleModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatInputModule, MatListModule, MatProgressBarModule, MatProgressSpinnerModule],
   declarations: [],
   providers: [{
     provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline', floatLabel: 'always' }
