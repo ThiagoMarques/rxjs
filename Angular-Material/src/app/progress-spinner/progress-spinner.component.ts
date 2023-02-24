7976d9b580d2f309a016c342110d972408afb229
ThiagoMarques <thiagossmarques@gmail.com>
2023-02-24 12:49:42 -0300

    feat: insere progressbar com RxJS

--- Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
+++ Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, map, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-progress-spinner',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-spinner.component.scss']
 })
 export class ProgressSpinnerComponent implements OnInit {
+  public loadingPercent = 50
 
   constructor() { }
 
   ngOnInit() {
+    this.loadingProgress(500).subscribe(i => this.loadingPercent = i)
+  }
+
+  loadingProgress(speed: number) {
+    /* interval: Função parecida com setTimeOut */
+    return interval(speed).pipe(
+      map(i => i * 5),
+      /* Realiza a desinscrição assim que o i for menor ou igual a 100 */
+      takeWhile(i => i <= 100)
+    )
   }
 
 }
